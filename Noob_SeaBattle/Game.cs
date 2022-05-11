035808558eb61c3cf196a0e4990a4c872e3dde5b
GmaB-a <vladik11012006@gmail.com>
2022-05-11 18:33:00 +0300

    a lot rewritten
    not done yet

--- Noob_SeaBattle/Game.cs
+++ Noob_SeaBattle/Game.cs
@@ -6,22 +6,19 @@ using System.Threading.Tasks;
 
 namespace Noob_SeaBattle
 {
-    class Player
-    {
-        public char[,] field;
-        public int shipCount;
-        public bool isBot;
-        public int playerNumber;
-    }
-
     internal class Game
     {
-        const int width = 2;
-        const int height = 2;
-        int maxAmountOfShipCells;
+        public const int width = 5;
+        public const int height = 5;
+        public const int maxAmountOfShipCells = 1 + (width * height) / 5;
         string letters = "abcdefghijklmnopqrstuvwxyz";
-        Player player1;
-        Player player2;
+
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player currentPlayer;
+        Player notCurrentPlayer;
+
+        Random rnd = new Random();
 
         public enum fieldCells
         {
@@ -31,101 +28,74 @@ namespace Noob_SeaBattle
             brokenShip = 'X'
         }
 
-        Random rnd = new Random();
         public void Play()
         {
-            maxAmountOfShipCells = 1 + (width * height) / 5;
-            Intro(out player1, out player2);
-            Player currentPlayer = player1;
-            Player notCurrentPlayer = player2;
+            player1 = player1.CreatePlayer();
+            Intro();
+            bool isOnlyBots = GetPlayMode();
+            player2 = player2.CreatePlayer();
+            (currentPlayer, notCurrentPlayer) = (player1, player2);
             while (!IsGameEnd())
             {
                 ShowBothFields(currentPlayer);
                 bool haveMissed;
                 int x, y;
-                if (!currentPlayer.isBot)
+                if (currentPlayer.isPlayer)
                 {
-                    (x, y) = GetPlayerShootPosition(notCurrentPlayer.field);
+                    (x, y) = GetPlayerShootPosition();
                 }
                 else
                 {
-                    (x, y) = GetRandomShootPosition(notCurrentPlayer.field);
+                    (x, y) = GetRandomShootPosition();
                 }
-                haveMissed = ShootAndCheckMiss(x, y, notCurrentPlayer, out notCurrentPlayer.shipCount);
-                if (haveMissed) (currentPlayer, notCurrentPlayer) = ChangeCurrentPlayer(currentPlayer, notCurrentPlayer);
+                haveMissed = ShootAndCheckMiss(x, y);
+                if (!currentPlayer.isPlayer) Console.ReadLine();
+                if (haveMissed) (currentPlayer, notCurrentPlayer) = (notCurrentPlayer, currentPlayer);
                 Console.Clear();
             }
         }
 
-        void Intro(out Player player1, out Player player2)
+
+        void Intro()
         {
-            player1 = CreatePlayer(1);
             Console.WriteLine("Ships: " + fieldCells.ship);
             Console.WriteLine("Broken ship: " + fieldCells.brokenShip);
             Console.WriteLine("Place, where you have already shot, but missed: " + fieldCells.miss);
             Console.WriteLine("Answer: first write a number representing height or y; after that write a letter, representing width or x");
             Console.WriteLine("Both numbers and letters you can see when the game has started");
-            Console.WriteLine("Press either 1 or 2 for different modes; 1 - you vs bot; 2 - you vs another player");
+            Console.WriteLine("Press either 0 or 1 or 2 for different modes; 0 - bot vs bot; 1 - you vs bot; 2 - you vs another player");
+        }
+
+
+        bool GetPlayMode()
+        {
             string playModeInString = Console.ReadLine();
-            player2 = CreatePlayer(2);
-            GetPlayMode(playModeInString, out player1, out player2);
-            Console.Clear();
-        }
-
-        Player CreatePlayer(int playerNumber)
-        {
-            Player player = new Player();
-            player.field = CreateOneField();
-            player.shipCount = maxAmountOfShipCells;
-            player.playerNumber = playerNumber;
-            return player;
-        }
-
-        void GetPlayMode(string playModeInString, out Player newPlayer1, out Player newPlayer2)
-        {
-            newPlayer1 = player1;
-            newPlayer2 = player2;
             int.TryParse(playModeInString, out int playMode);
-            newPlayer1.isBot = false;
-            if (playMode == 2) newPlayer2.isBot = false;
-            else newPlayer2.isBot = true;
+            SetPlayMode(playMode);
+            Console.Clear();
+            if (playMode == 0) return true;
+            return false;
         }
 
-        char[,] CreateOneField()
+        void SetPlayMode(int playMode)
         {
-            char[,] field = new char[width, height];
-            for (int y = 0; y < height; y++)
+            switch (playMode)
             {
-                for (int x = 0; x < width; x++)
-                {
-                    field[x, y] = (char)fieldCells.empty;
-                }
+                case 0: (player1.isPlayer, player2.isPlayer) = (false, false); break;
+                case 1:
+                    if (rnd.Next(0,100) < 50) (player1.isPlayer, player2.isPlayer) = (true, false);
+                    else (player1.isPlayer, player2.isPlayer) = (false, true);
+                    break;
+                case 2: (player1.isPlayer, player2.isPlayer) = (true, true); break;
             }
-
-            field = PlaceShips(field);
-            return field;
         }
 
-        char[,] PlaceShips(char[,] field)
+        /*(bool, bool) SetPlayMode(int playMode) => playMode switch
         {
-            for (int i = 0; i < maxAmountOfShipCells; i++)
-            {
-                int x = rnd.Next(0, width);
-                int y = rnd.Next(0, height);
-
-                if (field[x, y] != (char)fieldCells.empty) field[x, y] = (char)fieldCells.ship;
-                else
-                {
-                    while (field[x, y] == (char)fieldCells.ship)
-                    {
-                        x = rnd.Next(0, width);
-                        y = rnd.Next(0, height);
-                    }
-                    field[x, y] = (char)fieldCells.ship;
-                }
-            }
-            return field;
-        }
+            0 => (false, false),
+            1 => (true, false),
+            _ => (true, true)
+        }; */
 
         void ShowBothFields(Player currentPlayer)
         {
@@ -138,26 +108,38 @@ namespace Noob_SeaBattle
             {
                 if (y < 9) Console.Write(" ");
                 Console.Write(y + 1 + " ");
-                WriteThisChar(player1.field, y, player1.playerNumber == currentPlayer.playerNumber);
+                WriteThisChar(player1, y);
                 Console.Write(" | ");
-                WriteThisChar(player2.field, y, player2.playerNumber == currentPlayer.playerNumber);
+                WriteThisChar(player2, y);
                 Console.WriteLine();
             }
         }
 
-        void WriteThisChar(char[,] field, int y, bool isYou)
+        void WriteThisChar(Player player, int y)
         {
+            bool needToShowTile = needToShowThisTile(player);
             for (int x = 0; x < width; x++)
             {
-                if (!isYou)
-                {
-                    if (field[x, y] == (char)fieldCells.ship) Console.Write((char)fieldCells.empty);
-                    else Console.Write(field[x, y]);
-                    //Console.Write(field[x, y]);
-                }
-                else Console.Write(field[x, y]);
+                Console.Write(WriteTrueChar(player.field[x, y], needToShowTile));
             }
         }
+        bool needToShowThisTile(Player player)
+        {
+            if(!player1.isPlayer && !player2.isPlayer) return true;
+            if (player != currentPlayer) return false;
+
+            return true;
+        }
+
+        char WriteTrueChar(char ThisTile, bool needToShowTile)
+        {
+            if (ThisTile == (char)fieldCells.ship && !needToShowTile) 
+                return ((char)fieldCells.empty);
+
+            return ThisTile;
+        }
+
+
         void WriteThisLetter(int width)
         {
             for (int x = 0; x < width; x++)
@@ -166,35 +148,19 @@ namespace Noob_SeaBattle
             }
         }
 
-        (int, int) GetPlayerShootPosition(char[,] enemyField)
+        (int, int) GetPlayerShootPosition()
         {
-            Console.WriteLine("write a number");
-            string answerYinString = Console.ReadLine();
-            int answerY;
-            int.TryParse(answerYinString, out answerY);
-            answerY -= 1;
-            Console.WriteLine("write a letter");
-            char answerXinChar = Console.ReadKey().KeyChar;
             int answerX = width;
-            for (int i = 0; i < letters.Length; i++)
+            int answerY;
+            do
             {
-                if (answerXinChar == letters[i])
-                {
-                    answerX = i;
-                    break;
-                }
-            }
-            while (!CheckIfShootablePosition(answerX, answerY, enemyField))
-            {
-                Console.WriteLine();
                 Console.WriteLine("Write a number");
-                answerYinString = Console.ReadLine();
+                string answerYinString = Console.ReadLine();
                 int.TryParse(answerYinString, out answerY);
                 answerY -= 1;
 
                 Console.WriteLine("Write a letter");
-                Console.WriteLine();
-                answerXinChar = Console.ReadKey().KeyChar;
+                char answerXinChar = Console.ReadKey().KeyChar;
                 for (int i = 0; i < letters.Length; i++)
                 {
                     if (answerXinChar == letters[i])
@@ -203,58 +169,40 @@ namespace Noob_SeaBattle
                         break;
                     }
                 }
-            }
+            } while (!CheckIfShootablePosition(answerX, answerY));
             return (answerX, answerY);
         }
 
-        bool CheckIfShootablePosition(int answerX, int answerY, char[,] enemyField)
+        bool CheckIfShootablePosition(int x, int y)
         {
-            if (answerY < 0) return false;
-            else if (answerY >= height) return false;
-            else if (answerX >= width) return false;
-            else if (enemyField[answerX, answerY] == (char)fieldCells.brokenShip) return false;
-            else if (enemyField[answerX, answerY] == (char)fieldCells.miss) return false;
+            if (y < 0 || y >= height) return false;
+            if (x >= width) return false;
+            if (notCurrentPlayer.field[x, y] == (char)fieldCells.brokenShip) return false;
+            if (notCurrentPlayer.field[x, y] == (char)fieldCells.miss) return false;
             return true;
         }
 
-        bool ShootAndCheckMiss(int x, int y, Player enemy, out int newEnemyShipCount)
+        bool ShootAndCheckMiss(int x, int y)
         {
-            newEnemyShipCount = enemy.shipCount;
-            if (enemy.field[x, y] == (char)fieldCells.ship)
+            if (notCurrentPlayer.field[x, y] == (char)fieldCells.ship)
             {
-                enemy.field[x, y] = (char)fieldCells.brokenShip;
-                newEnemyShipCount -= 1;
+                notCurrentPlayer.field[x, y] = (char)fieldCells.brokenShip;
+                notCurrentPlayer.shipCount -= 1;
                 return false;
             }
-            else if (enemy.field[x, y] == (char)fieldCells.empty) enemy.field[x, y] = (char)fieldCells.miss;
+            else if (notCurrentPlayer.field[x, y] == (char)fieldCells.empty) notCurrentPlayer.field[x, y] = (char)fieldCells.miss;
             return true;
         }
 
-        (int, int) GetRandomShootPosition(char[,] enemyField)
+        (int, int) GetRandomShootPosition()
         {
-            int x = rnd.Next(0, width);
-            int y = rnd.Next(0, height);
-            while (!CheckIfShootablePosition(x, y, enemyField))
-            {
+            int x;
+            int y;
+            do{
                 x = rnd.Next(0, width);
                 y = rnd.Next(0, height);
-            }
+            } while (!CheckIfShootablePosition(x, y));
             return (x, y);
-        }
-
-        (Player, Player) ChangeCurrentPlayer(Player currentPlayer, Player notCurrentPlayer)
-        {
-            if (currentPlayer.playerNumber == player1.playerNumber)
-            {
-                currentPlayer = player2;
-                notCurrentPlayer = player1;
-            }
-            else
-            {
-                currentPlayer = player1;
-                notCurrentPlayer = player2;
-            }
-            return (currentPlayer, notCurrentPlayer);
         }
 
         bool IsGameEnd()
---
+++ Noob_SeaBattle/Player.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noob_SeaBattle
+{
+    internal class Player
+    {
+        public char[,] field;
+        public int shipCount;
+        public bool isPlayer;
+
+        const int width = Game.width;
+        const int height = Game.height;
+        const int maxAmountOfShipCells = Game.maxAmountOfShipCells;
+        Random rnd;
+
+        enum fieldCells
+        {
+            empty = Game.fieldCells.empty,
+            ship = Game.fieldCells.ship,
+            miss = Game.fieldCells.miss,
+            brokenShip = Game.fieldCells.brokenShip
+        }
+
+        public Player CreatePlayer()
+        {
+            rnd = new Random();
+            Player player = new Player();
+            player.field = CreateOneField();
+            player.shipCount = maxAmountOfShipCells;
+            return player;
+        }
+
+
+        char[,] CreateOneField()
+        {
+            char[,] field = new char[width, height];
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    field[x, y] = (char)fieldCells.empty;
+                }
+            }
+
+            field = PlaceShips(field);
+            return field;
+        }
+
+        char[,] PlaceShips(char[,] field)
+        {
+            int x;
+            int y;
+            for (int i = 0; i < Game.maxAmountOfShipCells; i++)
+            {
+                do{
+                    x = rnd.Next(0, width);
+                    y = rnd.Next(0, height);
+                } while (field[x, y] == (char)fieldCells.ship);
+                field[x, y] = (char)fieldCells.ship;
+            }
+            return field;
+        }
+    }
+}
